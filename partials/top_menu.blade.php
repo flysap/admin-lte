7e89a783455a3a636a1b81ee24fb8e573a8d0539
Radu <radu.bordei89@gmail.com>
2015-10-19 11:26:12 +0300

    switch collapsed panel.

--- partials/top_menu.blade.php
+++ partials/top_menu.blade.php
@@ -12,7 +12,7 @@
     <!-- Header Navbar -->
     <nav class="navbar navbar-static-top" role="navigation">
         <!-- Sidebar toggle button-->
-        <a href="#" class="sidebar-toggle" data-toggle="offcanvas" role="button">
+        <a href="#" class="sidebar-toggle" data-toggle="offcanvas" role="button" onclick="switchCollapsed();">
             <span class="sr-only">Toggle navigation</span>
         </a>
         <!-- Navbar Right Menu -->
