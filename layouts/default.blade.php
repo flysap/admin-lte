0123518910d8afcefd99136a5c5836e6c0044781
Radu <radu.bordei89@gmail.com>
2015-10-19 12:48:31 +0300

    fix paths.

--- layouts/default.blade.php
+++ layouts/default.blade.php
@@ -21,13 +21,14 @@ scratch. This page gets rid of all links and provides the needed markup only.
           page. However, you can choose any other skin. Make sure you
           apply the skin class to the body tag so the changes take effect.
     -->
-
     <link href="{{ asset ("/bower_components/adminLTE/dist/css/skins/skin-black.min.css") }}" rel="stylesheet" type="text/css" />
-    <link href="{{ asset ("/bower_components/adminLTE/dist/jquery-ui.min.css") }}" rel="stylesheet" type="text/css" />
 
-    <!-- jQuery 2.1.4 -->
-    <script src="/bower_components/adminLTE/dist/jquery.min.js" type="text/javascript"></script>
-    <script src="/bower_components/adminLTE/dist/jquery-ui.min.js" type="text/javascript"></script>
+
+    <script src="{{ asset ("/bower_components/jquery/dist/jquery.min.js") }}" type="text/javascript"></script>
+    <script src="{{ asset ("/bower_components/jquery-ui/jquery-ui.min.js") }}" type="text/javascript"></script>
+    <link href="{{ asset ("/bower_components/jquery-ui/themes/base/jquery-ui.min.css") }}" rel="stylesheet" type="text/css" />
+
+
     <script src="//tinymce.cachefly.net/4.2/tinymce.min.js"></script>
 
     <!-- HTML5 Shim and Respond.js IE8 support of HTML5 elements and media queries -->
